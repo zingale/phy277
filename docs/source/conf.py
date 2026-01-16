475a9022133e9f81e69b563211e85f56ec92b340
Michael Zingale <michael.zingale@stonybrook.edu>
2026-01-16 10:39:27 -0500

    try again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -170,5 +170,4 @@ linkcheck_retries = 3
 linkcheck_timeout = 100
 linkcheck_allow_unauthorized = True
 linkcheck_ignore = [r"https://dl.acm.org/doi/10.1145/103162.103163"]
-user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0"
 
