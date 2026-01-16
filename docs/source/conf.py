405924b13d57e3375c980d55669e83253527cf29
Michael Zingale <michael.zingale@stonybrook.edu>
2026-01-16 10:36:29 -0500

    some linkcheck options

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -163,3 +163,12 @@ html_static_path = ['_static']
 
 html_css_files = ["myfile.css"]
 
+
+# -- Options for linkcheck
+
+linkcheck_retries = 3
+linkcheck_timeout = 100
+linkcheck_allow_unauthorized = True
+linkcheck_ignore = [r"https://dl.acm.org/doi/10.1145/103162.103163"]
+user_agent = "Mozilla/5.0 (X11; Linux x86_64; rv:25.0) Gecko/20100101 Firefox/25.0"
+
