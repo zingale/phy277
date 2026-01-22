5bab8af0ea564df80de8b6ea651c9ec5488d459f
Michael Zingale <michael.zingale@stonybrook.edu>
2026-01-22 08:45:26 -0500

    update example

--- examples/floating_point/comparing.cpp
+++ examples/floating_point/comparing.cpp
@@ -2,14 +2,28 @@
 
 int main() {
 
-    double h{0.01};
+    double h{0.1};
     double sum{0.0};
 
-    for (int n = 0; n < 100; ++n) {
-        sum += h;
-    }
+    // later well learn how to do a loop, but for now
+    // we'll explicitly add 10 h's
 
-    std::cout << "sum == 1:        " << (sum == 1.0) << std::endl;
-    std::cout << "|sum - 1| < tol: " << (std::abs(sum - 1.0) < 1.e-10) << std::endl;
+    sum += h;
+    sum += h;
+    sum += h;
+    sum += h;
+    sum += h;
+    sum += h;
+    sum += h;
+    sum += h;
+    sum += h;
+    sum += h;
+
+    std::cout << "sum == 1:        "
+              << (sum == 1.0) << std::endl;
+
+    std::cout << "|sum - 1| < tol: "
+              << (std::abs(sum - 1.0) < 1.e-10)
+              << std::endl;
 
 }
