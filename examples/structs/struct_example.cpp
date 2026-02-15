699a81c49af56e9951c24a16738f7f3eeafa6393
Michael Zingale <michael.zingale@stonybrook.edu>
2026-02-15 09:05:27 -0500

    more updates

--- examples/structs/struct_example.cpp
+++ examples/structs/struct_example.cpp
@@ -12,15 +12,16 @@ struct Planet
 
 int main() {
 
-    const std::vector<Planet> planets {{.name="Mercury",  .a=0.3871, .e=0.2056},
-                                       {.name="Venus",    .a=0.7233, .e=0.0068},
-                                       {.name="Earth",    .a=1.0000, .e=0.0167},
-                                       {.name="Mars",     .a=1.5237, .e=0.0934},
-                                       {.name="Jupiter",  .a=5.2029, .e=0.0484},
-                                       {.name="Saturn",   .a=9.5370, .e=0.0539},
-                                       {.name="Uranus",  .a=19.189,  .e=0.0473},
-                                       {.name="Neptune", .a=30.070,  .e=0.0086}};
-    for (auto p : planets) {
+    const std::vector<Planet> planets{{.name="Mercury", .a=0.3871, .e=0.2056},
+                                      {.name="Venus",   .a=0.7233, .e=0.0068},
+                                      {.name="Earth",   .a=1.0000, .e=0.0167},
+                                      {.name="Mars",    .a=1.5237, .e=0.0934},
+                                      {.name="Jupiter", .a=5.2029, .e=0.0484},
+                                      {.name="Saturn",  .a=9.5370, .e=0.0539},
+                                      {.name="Uranus",  .a=19.189, .e=0.0473},
+                                      {.name="Neptune", .a=30.070, .e=0.0086}};
+
+    for (const auto& p : planets) {
         std::cout << std::format("{} has a period of {:.2f} years\n",
                                  p.name, std::sqrt(std::pow(p.a, 3)));
     }
