406dc1b0eb84415db3fbcd3d2dd53e52a0e5c03a
Michael Zingale <michael.zingale@stonybrook.edu>
2026-04-20 09:24:06 -0400

    add a private example

---
+++ examples/classes/simple-class-private.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <numbers>
+#include <format>
+
+class Circle {
+
+    // member data
+    double radius{};
+
+public:
+
+    // constructor
+    Circle(double r) {
+        radius = r;
+    }
+
+    // member functions
+    double r() {
+        return radius;
+    }
+
+    double circumference() {
+        return 2.0 * std::numbers::pi * radius;
+    }
+
+    double area() {
+        return std::numbers::pi * radius * radius;
+    }
+
+};
+
+int main() {
+
+    Circle c(3.0);
+    std::cout
+        << std::format("circle: radius = {:6.3f}; C = {:6.3f}; A = {:6.3f}\n",
+                       c.r(), c.circumference(), c.area());
+
+}
